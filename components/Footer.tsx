3d93929f3bd0a2f388fd1134ac424683627c2d9a
AntonR-github <antonr1wk@gmail.com>
2023-07-15 15:58:01 +0300

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
-// import { footerLinks } from "@constants";
+import { footerLinks } from "../constants/constants"
 
 const Footer = () => {
   return (
     <footer className='flex flex-col text-black-100  mt-5 border-t border-gray-100'>
-    {/* <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+    <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
       <div className='flex flex-col justify-start items-start gap-6'>
         <Image src='/logo.svg' alt='logo' width={118} height={18} className='object-contain' />
         <p className='text-base text-gray-700'>
@@ -46,7 +46,7 @@ const Footer = () => {
           Terms & Condition
         </Link>
       </div>
-    </div> */}
+    </div>
   </footer>
   )
 }
